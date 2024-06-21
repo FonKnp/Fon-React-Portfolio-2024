369e1158b7ae88c412a38f8942c5fb6e52bd186d
FonKnp <fonnfonk@gmail.com>
2024-06-20 20:23:10 -0400

    created router

--- Main/src/main.jsx
+++ Main/src/main.jsx
@@ -1,8 +1,37 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+
 import './index.css'
 
+import App from './App';
+import Error from './pages/Error';
+import Home from './pages/Home';
+import About from './pages/About';
+import Contact from './pages/Contact';
+
+const router = createBrowserRouter([
+    {
+      path: '/',
+      element: <App />,
+      errorElement: <Error />,
+      children: [
+        {
+          index: true,
+          element: <Home />,
+        },
+        {
+          path: '/About',
+          element: <About />,
+        },
+        {
+          path: '/Contact',
+          element: <Contact />,
+        },
+      ],
+    },
+  ]);
+
 ReactDOM.createRoot(document.getElementById('root')).render(
-    <App />
-)
+    <RouterProvider router={router} />
+);
