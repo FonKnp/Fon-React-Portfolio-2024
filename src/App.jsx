5dc3f80d73c60c2f89d4fd31accb82b60455a46f
FonKnp <fonnfonk@gmail.com>
2024-06-23 14:09:55 -0400

    move folder

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import NavBar from './components/Navigation/Navbar';
+
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <NavBar />
+
+    </>
+  )
+}
+
+export default App
